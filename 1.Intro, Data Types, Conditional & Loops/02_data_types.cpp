2363394cdcb99c3242a29626d9eacb109e226594
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:50:35 +0530

    Create 02_data_types.cpp

---
+++ 1.Intro, Data Types, Conditional & Loops/02_data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 123;
+    cout<< a << endl;
+
+    char b = 'v';
+    cout<< b <<endl;
+
+    bool bl = true;
+    cout<< bl<<endl;
+
+    float f = 1.2;
+    cout<< f << endl;
+
+    int size = sizeof(a);
+    cout<< "Size of a is :" << size << endl;
+}
