d61d6dd879d5405d754c37aa96c3022eb5b31d15
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:05:30 +0530

    Create 09_find_square_root.cpp

---
+++ 14.Searching & Sorting/09_find_square_root.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int findSqrt(int n){
+    int s = 0;
+    int e = n;
+    int mid = s + (e-s)/2;
+    
+    int target = n;
+    int ans = -1;
+    
+    while(s<=e){
+        if(mid*mid > target){
+            e = mid-1;
+        }
+        else if(mid*mid < target){
+            ans = mid;
+            s = mid+1;
+        }
+        else{
+            return mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number: " << endl;
+    cin >> n;
+
+    int ans = findSqrt(n);
+    // cout << "Ans is: " << ans << endl;
+
+    int precision;
+    cout << "Enter the number of floating digits in precision: "<< endl;
+    cin >> precision;
+
+    double step = 0.1;
+    double finalAns = ans;
+    for(int i=0; i<precision;i++){
+        for(double j=finalAns; j*j <= n; j = j+step){
+            finalAns = j;
+        }
+        step = step / 10;
+    }
+
+    cout << "Final ans is: " << finalAns << endl;
+
+    return 0;
+}
