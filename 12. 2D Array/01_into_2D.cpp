c35e3ee445ae3353896e4e386e3d8cad25c8e19b
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:04:03 +0530

    Create 01_into_2D.cpp

---
+++ 12. 2D Array/01_into_2D.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // declare 2D array
+    int arr[3][3];
+
+    //initialisation
+    int brr[3][3] ={{1,2,3},{4,5,6},{7,8,9}};
+
+    cout << brr[1][3] << endl;
+    return 0;
+}
