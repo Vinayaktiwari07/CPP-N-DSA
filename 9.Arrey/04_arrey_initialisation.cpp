415274c5dbdafe4c3d2992910607ace90844fbc3
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:02:57 +0530

    Create 04_arrey_initialisation.cpp

---
+++ 9.Arrey/04_arrey_initialisation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int brr[5] = {1,2,3,4,5};
+    int crr[5] = {1,2};
+    int drr[2] = {1,2,3,4,5}; // Ye error dega bcoz of excess value.
+    
+    return 0;
+}
