4852292aeb6ba0ae5164258ec1e433174e3d5937
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:00:26 +0530

    Create 04_explicit_type_casting.cpp

---
+++ 5.Type Casting/04_explicit_type_casting.cpp
@@ -0,0 +1,12 @@
+// Explicit conversion from double to int
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    double pi = 3.14159265;
+    int intPi = (int)pi;
+    cout << intPi << endl;
+
+    return 0;
+}
