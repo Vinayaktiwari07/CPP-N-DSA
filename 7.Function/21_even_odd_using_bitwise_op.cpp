6398c18cf457cb305b462128df72baa3c8453bbd
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:02:07 +0530

    Create 21_even_odd_using_bitwise_op.cpp

---
+++ 7.Function/21_even_odd_using_bitwise_op.cpp
@@ -0,0 +1,17 @@
+// Checking Even Odd by using Bit wise Operator.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num & 1)
+        cout << num << " is Odd" << endl;
+    else
+        cout << num << " is Even" << endl;
+
+    return 0;
+}
