3e18da4d4e37625341901f2f667f96e87d865010
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:00:15 +0530

    Create 01_implicit_type_casting.cpp

---
+++ 5.Type Casting/01_implicit_type_casting.cpp
@@ -0,0 +1,12 @@
+// Implicit converision from int to float
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1 = 10;
+    float num2 = 5.5;
+    float result = num1+num2;
+    cout << result << endl;
+    return 0;
+}
