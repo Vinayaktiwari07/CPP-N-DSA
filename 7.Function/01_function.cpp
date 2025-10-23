44083f149a3d00ddda2b2fa0541bc5c506f96da3
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:00:50 +0530

    Create 01_function.cpp

---
+++ 7.Function/01_function.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+// Function Declaration
+void printline();
+
+
+int main(){
+    
+    //Function call
+    printline();
+    
+    return 0; 
+}
+
+
+// Function Definataion
+void printline(){
+    for(int i=0; i<10; i++){
+        cout << "Sundar ko Sundari pasand hai" << endl;
+    }
+}
