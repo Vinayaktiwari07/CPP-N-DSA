fe5b0d540e404bcf55c69ec154f2386e5a8afaf0
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:02:48 +0530

    Create 01_intro_arrey.cpp

---
+++ 9.Arrey/01_intro_arrey.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Arrey Creation
+    int arr[101];
+    char ch[102];
+    bool flags[222];
+    long num[897];
+    short snum[909];
+
+
+    cout << "Arrey created Successfully" << endl;
+
+    return 0;
+}
