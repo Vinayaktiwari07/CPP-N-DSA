9961e322c1efa6a25135ddf4e54484ec25d68714
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:54:48 +0530

    Create 06_pattern_square.cpp

---
+++ 3.Conditions & Loops/06_pattern_square.cpp
@@ -0,0 +1,14 @@
+// Method 1 for printing Square
+
+
+#include<iostream>
+using namespace std;
+
+ int main(){
+
+    for(int i = 1; i <= 4; i++){
+        cout << "* * * *" << endl;
+    }
+
+    return 0;
+}
