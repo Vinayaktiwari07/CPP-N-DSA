501357e7271da4551504b401bb839600c616fb8e
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:04:10 +0530

    Create 03_print_col_wise.cpp

---
+++ 12. 2D Array/03_print_col_wise.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // declare 2D array
+    int arr[3][3];
+
+    //initialisation
+    int brr[3][3] ={{1,2,3},{4,5,6},{7,8,9}};
+    int n=3;
+    for(int row=0;row<n; row++){
+        for(int col=0;col<n;col++){
+            cout << brr[col][row] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
