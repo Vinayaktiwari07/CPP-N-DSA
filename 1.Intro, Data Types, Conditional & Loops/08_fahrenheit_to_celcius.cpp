bcf8dc4e7bc7268722b92780217ddd4365d3a7e8
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:50:58 +0530

    Create 08_fahrenheit_to_celcius.cpp

---
+++ 1.Intro, Data Types, Conditional & Loops/08_fahrenheit_to_celcius.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int f;
+    cin>>f;
+
+    int c = ((f-32)*5)/9;
+    cout<<"Value of fahrenheit to celcius is "<<c<<endl;
+    return 0;
+
+}
