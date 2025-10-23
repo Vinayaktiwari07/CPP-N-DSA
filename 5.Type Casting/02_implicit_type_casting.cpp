7fc485605c8ba374f32fc8241b8f423e9541dabe
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:00:18 +0530

    Create 02_implicit_type_casting.cpp

---
+++ 5.Type Casting/02_implicit_type_casting.cpp
@@ -0,0 +1,11 @@
+// Implicit conversion from char to int
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch = 'A';
+    char a = ch + 1;
+    cout << a << endl;
+    return 0;
+}
