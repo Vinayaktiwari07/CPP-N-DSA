4be7b8b2749623055e52e1440a9478c20481a644
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:59:56 +0530

    Create 06_pre_decrement.cpp

---
+++ 4.Bitwise Operators & Loops/06_pre_decrement.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+    cout << (--n) << endl;     //ye no. me pahle 1 subt. krega then print krega.
+    cout << n << endl;
+    return 0;
+}
