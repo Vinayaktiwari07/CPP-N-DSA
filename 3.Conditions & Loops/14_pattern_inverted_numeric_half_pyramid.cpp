6284fc3bb2b855466891f17d89266abc40b8f71f
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:55:21 +0530

    Create 14_pattern_inverted_numeric_half_pyramid.cpp

---
+++ 3.Conditions & Loops/14_pattern_inverted_numeric_half_pyramid.cpp
@@ -0,0 +1,16 @@
+// Printing Inverted Numeric Half Pyramid
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=10;
+
+    for(int row=0; row<n; row++){
+        for(int col=0; col<n-row; col++){
+            cout << col+1 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
