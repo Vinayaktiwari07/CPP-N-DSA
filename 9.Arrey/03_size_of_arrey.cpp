16febc87f4dd11c03ace66eae5924f7ea615ef4c
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:02:54 +0530

    Create 03_size_of_arrey.cpp

---
+++ 9.Arrey/03_size_of_arrey.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+
+    int a =5;
+    cout << "Size of a is: "<< sizeof(a) << endl;
+
+    int arr[14];
+    cout << "Size of arr is: " << sizeof(arr) << endl;
+
+    return 0;
+}
