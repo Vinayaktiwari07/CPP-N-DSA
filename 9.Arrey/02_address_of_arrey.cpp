26a75de084dc7cd036fb1ff9f4ed5b9231b622e4
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:02:51 +0530

    Create 02_address_of_arrey.cpp

---
+++ 9.Arrey/02_address_of_arrey.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a =5;
+    cout << "Address of a is: " << &a << endl;
+
+
+    int arr[12];
+    cout << "Starting/Base Address of arr is: " << &arr << endl;
+
+    return 0;
+
+}
