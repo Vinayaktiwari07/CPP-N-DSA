c797cda781cdc2e75169f0915aabc5dde30e5771
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:56:30 +0530

    Create 30_pattern_Floyd_triangle.cpp

---
+++ 3.Conditions & Loops/30_pattern_Floyd_triangle.cpp
@@ -0,0 +1,18 @@
+// Printing Floy'd Triangle.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    int count = 1;
+
+    for(int row=0; row<n; row++){
+        for(int col=0; col<row+1;col++){
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
