dcb2f032fac35825676d04acf1187fffaed71934
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:00:00 +0530

    Create 07_post_decrement.cpp

---
+++ 4.Bitwise Operators & Loops/07_post_decrement.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+    cout << (n--) << endl;     // yaha print krne ke baad subtracy krega.
+    cout << n << endl;
+    return 0;
+}
