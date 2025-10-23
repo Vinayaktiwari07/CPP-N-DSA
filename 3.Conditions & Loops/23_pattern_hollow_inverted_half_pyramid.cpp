c0333f2c57f034189231c2fc6c0cfb593b04e6c3
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:56:01 +0530

    Create 23_pattern_hollow_inverted_half_pyramid.cpp

---
+++ 3.Conditions & Loops/23_pattern_hollow_inverted_half_pyramid.cpp
@@ -0,0 +1,25 @@
+// Printing Hollow Inverted Half Pyramid
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 6;
+    for(int row=0; row<n; row++){
+        for(int col=0; col<n-row; col++){
+            if(row == 0 || row == n-1){
+                cout << "*";
+            }
+            else{
+                if(col == 0 || col == n-row-1){
+                    cout << "*";
+                }
+                else{
+                    cout << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;   
+}
