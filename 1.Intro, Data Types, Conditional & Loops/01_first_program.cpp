cb7341ad37e2013799dd0bfeb69affe4326392c7
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:48:06 +0530

    Create 01_first_program.cpp

---
+++ 1.Intro, Data Types, Conditional & Loops/01_first_program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout <<"Hello World" << endl;
+    cout <<"Hello Siri"<< '\n';
+    cout <<"Hey Siri\n";
+    
+    cout <<"Hello Duniya";
+    cout << "\n";
+}
