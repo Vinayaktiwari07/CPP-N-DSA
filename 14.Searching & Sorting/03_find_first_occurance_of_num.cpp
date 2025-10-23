6b0ec7f87865274cfba53e43d22bd8548a893ca1
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 20:05:08 +0530

    Create 03_find_first_occurance_of_num.cpp

---
+++ 14.Searching & Sorting/03_find_first_occurance_of_num.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int firstOccurence(vector<int> arr, int target){
+    int s = 0;
+    int e = arr.size() - 1;
+
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    
+    while(s <= e){
+        if(arr[mid] == target){
+            //ans store then go left
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(target > arr[mid]){
+            // right me search
+            s = mid + 1;
+        }
+        else if(target < arr[mid]){
+            //left search
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> v{1,2,3,4,4,4,4,4,6,7};
+    int target = 4;
+    int indexOfFirstOcc = firstOccurence(v, target);
+
+    cout << "ans is " << indexOfFirstOcc << endl;
+    return 0;
+}
