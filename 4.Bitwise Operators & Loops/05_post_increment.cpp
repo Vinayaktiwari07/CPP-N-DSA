cb2aaf9e834a1c331724680ecc5f32783a08cc2e
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:59:51 +0530

    Create 05_post_increment.cpp

---
+++ 4.Bitwise Operators & Loops/05_post_increment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+    cout << (n++) << endl;     // yaha no. ko print krega then add on krega.
+    cout << n << endl;
+    return 0;
+}
