8664f9141837638c08e489451e9f926f71ae729f
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:59:40 +0530

    Create 02_left_shift_operator.cpp

---
+++ 4.Bitwise Operators & Loops/02_left_shift_operator.cpp
@@ -0,0 +1,11 @@
+// Left shift operator
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 7;
+    cout << (n << 1) << endl;  //Left shift me number * 2^no.of shift hota hai
+
+    return 0;
+}
