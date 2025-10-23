6313d8b53a1253c2589df017a0bcbc9656ac1ca8
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:59:15 +0530

    Create 06_triplet_sum.cpp

---
+++ 10.Vector/06_triplet_sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+    vector<int> arr{1,3,5,7,2,4,6};
+    int sum = 9;
+    // outer loop will traverse for each element
+    for(int i=0; i<arr.size();i++){
+        for(int j=i+1; j<arr.size(); j++){
+            for(int k=j+1; k<arr.size(); k++){
+                if(arr[i]+arr[j]+arr[k] == sum){
+                    cout << arr[i] <<"," << arr[j] <<"," <<arr[k] << endl; 
+                }
+
+            }
+           
+        }
+    }
+    return 0;
+}
