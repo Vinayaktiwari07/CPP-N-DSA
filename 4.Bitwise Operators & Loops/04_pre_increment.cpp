7271f1ba212219572642fac6aed6f808503d8a57
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:59:48 +0530

    Create 04_pre_increment.cpp

---
+++ 4.Bitwise Operators & Loops/04_pre_increment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+    cout << (++n) << endl;     // ye n=4 me 1 add kr dega or sore kr dega, print krne pr 5 hoga.
+    cout << n << endl;
+    return 0;
+}
