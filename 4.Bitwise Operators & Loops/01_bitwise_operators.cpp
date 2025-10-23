4b81b20c5cd7a989ae1190ae21bbe3b13ddb544a
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:59:37 +0530

    Create 01_bitwise_operators.cpp

---
+++ 4.Bitwise Operators & Loops/01_bitwise_operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 2;
+    int b = 3;
+
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    cout << (a ^ b) << endl;
+
+
+    int num=1;
+    cout << ~num << endl;
+    cout << (~num) << endl;
+    cout << ~(num) << endl;
+
+    return 0; 
+}
