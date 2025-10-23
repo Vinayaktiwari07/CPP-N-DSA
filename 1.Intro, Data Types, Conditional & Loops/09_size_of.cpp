210b64bdc776aac31f88c0b7126d8686d8cca31b
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 19:51:03 +0530

    Create 09_size_of.cpp

---
+++ 1.Intro, Data Types, Conditional & Loops/09_size_of.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int num = 50;
+    // cout << num << endl;
+
+    // integer 
+    cout << sizeof(int) << endl;
+    
+    // character 
+    cout << sizeof(char) << endl;
+
+    // float 
+    cout << sizeof(float) << endl;
+
+    // double 
+    cout << sizeof(double) << endl;
+
+    return 0;
+}
