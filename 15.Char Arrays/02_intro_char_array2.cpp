6b9855a80ff9a1913cd17cdc8a471e0db1924cf7
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-12-08 10:05:04 +0530

    Create 02_intro_char_array2.cpp

---
+++ 15.Char Arrays/02_intro_char_array2.cpp
@@ -0,0 +1,15 @@
+// Taking input index wise -> one by one.
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    char ch[100];
+
+    ch[0] = 'a';
+    ch[1] = 'b';
+    cin >> ch[2];
+
+    cout << ch[0] << ch[1] << ch[2] << endl;
+    return 0;
+}
