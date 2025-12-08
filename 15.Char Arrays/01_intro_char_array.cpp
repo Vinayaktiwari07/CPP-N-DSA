2acae510df4b3960a358f976081c34ee591d2d42
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-12-08 10:04:55 +0530

    Create 01_intro_char_array.cpp

---
+++ 15.Char Arrays/01_intro_char_array.cpp
@@ -0,0 +1,12 @@
+// Sequencial input -> all input at a time.
+
+#include<iostream>
+using namespace std;
+int main() {
+    char name[100];
+    cout << "Enter your Name: " << endl;
+    cin >> name;
+
+    cout << "Aapka naam " << name << " hai." << endl;
+    return 0;
+}
