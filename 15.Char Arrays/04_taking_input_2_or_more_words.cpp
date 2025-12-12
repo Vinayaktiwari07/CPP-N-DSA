2935972d694e2c0ab605153dc456726fabe1801e
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-12-12 19:26:18 +0530

    Create 04_taking_input_2_or_more_words.cpp

---
+++ 15.Char Arrays/04_taking_input_2_or_more_words.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    char arr[100];
+    cout << "Enter your full name here: ";
+    cin.getline(arr, 50);  // by default ye 'ENTER' pr input lena band kr deta hai.
+
+    cout << arr;
+
+    return 0;
+}
