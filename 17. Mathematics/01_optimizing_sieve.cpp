7dad3f9fc40e54b973c515cecc5619a66fb3af0a
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2026-04-10 10:00:31 +0530

    Create 01_optimizing_sieve.cpp

---
+++ 17. Mathematics/01_optimizing_sieve.cpp
@@ -0,0 +1,35 @@
+// Sieve is used to find all the prime number upto n natural number
+// here we use ERATOSTHENES theorem
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<bool> Sieve(int n){
+    // create a sieve array of N size telling isPrime.
+    vector<bool>sieve(n+1, true);
+    sieve[0] = sieve[1] = false;
+
+    for(int i=2;i*i<=n; i++){ // optimization 2
+        if(sieve[i] == true){
+            // int j=i*2;
+            int j = i*i; // optimization 1
+            //first unmarked number would be i*i, as others have been marked by 2 to (i - 1)
+            while(j<=n){
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+    return sieve;
+}
+
+int main() {
+    vector<bool> sieve = Sieve(25);
+    for(int i=0; i<=25; i++){
+        if(sieve[i]){
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
