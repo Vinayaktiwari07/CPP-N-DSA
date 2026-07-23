d3cdeaeb3194af3a34023c6bac6e9412e5a3f0ff
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2026-07-23 09:27:26 +0530

    Create 19_print_all_subarrays_using_recursion.cpp

---
+++ 18. Recursion/19_print_all_subarrays_using_recursion.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void printSubArray_util(vector<int> &nums, int start, int end){
+    //base case
+    if(end == nums.size()){
+        return;
+    }
+
+    // 1 case solution
+    for(int i=start; i<= end; i++){
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    // RE
+    printSubArray_util(nums, start, end+1);
+}
+void printSubArray(vector<int> &nums){
+    for(int start=0; start<nums.size(); start++){
+        int end = start;
+        printSubArray_util(nums, start, end);
+    }
+}
+
+int main() {
+    vector<int> nums{1,2,3,4,5};
+    printSubArray(nums);
+    return 0;
+}
