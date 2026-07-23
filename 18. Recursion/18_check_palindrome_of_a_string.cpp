019226c657f2a8d6666e9d2e20d27d4d014288f4
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2026-07-23 09:27:22 +0530

    Create 18_check_palindrome_of_a_string.cpp

---
+++ 18. Recursion/18_check_palindrome_of_a_string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string &s, int start, int end){
+    //base case
+    if(start >= end){
+        return true;
+    }
+
+    // ek case solution
+    if(s[start] != s[end]){
+        return false;
+    }
+
+    //RE
+    return isPalindrome(s, start+1, end-1);
+}
+
+int main() {
+    string s;
+    cout << "Enter your string here: ";
+    cin >> s;
+    cout << isPalindrome(s, 0, s.size()-1) << endl;
+    return 0;
+}
